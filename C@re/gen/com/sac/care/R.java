773b4c2f2cd45ee08689835f0855b21aa45d3a9b
sachinkumarbit <sachinkumarbit@gmail.com>
2014-04-19 21:22:47 +0530

    login

--- C@re/gen/com/sac/care/R.java
+++ C@re/gen/com/sac/care/R.java
@@ -31,6 +31,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int login=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
